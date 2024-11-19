4d452ec6e73d4080f41b75666f3e0277fecb38d0
liam123kxnx <linafriday10@gmail.com>
2024-11-19 21:06:10 +0100

    ..fun

---
+++ main.py
@@ -0,0 +1,16 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+  c = 200
+ d = 33
+if d > c:
+  print("d is greater than c")
+elif c == d:
+   print("c and d are equal")
+else:
+  print("c is greater than d")
